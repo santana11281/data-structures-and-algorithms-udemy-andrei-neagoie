d0da54d2dd561d0f9fe885d47e622ba8a2d9a1e1
Michael <santana11281@gmail.com>
2021-09-03 19:03:46 -0400

    c

---
+++ 08 - Data Structures Graph/01 - Graph .js
@@ -0,0 +1,41 @@
+
+ class Graph {
+  constructor() {
+    this.numberOfNodes = 0; 
+    this.adjacentList = {}; 
+  }
+
+  addVertex(node) {
+
+
+  }
+
+  addEdge(node1, node2) {
+
+    
+  }
+
+  showConnections() { 
+    const allNodes = Object.keys(this.adjacentList); 
+    for (let node of allNodes) { 
+      let nodeConnections = this.adjacentList[node]; 
+      let connections = ""; 
+      let vertex;
+      for (vertex of nodeConnections) {
+        connections += vertex + " ";
+      } 
+      console.log(node + "-->" + connections); 
+    } 
+} 
+ }
+
+
+ let myGraph = new Graph()
+
+
+ console.log(myGraph)
+
+
+
+
+
